29844083eac6b61a958c11ffb358450a839552db
s-CCOURAGE <s-CCOURAGE@484E293869.lwsd.k12wa.org>
2022-05-13 14:47:59 -0700

    hey ya!

---
+++ FuncPlotter/src/Function.java
@@ -0,0 +1,6 @@
+// defauly function interface
+public interface Function {
+
+	public double calc(double x);
+	
+}
--- FuncPlotter/src/Program.java
+++ FuncPlotter/src/Program.java
@@ -14,15 +14,70 @@ public class Program {
         canvas.setRange(-360, -240, 360, 240);
         canvas.open();
 
-        // Draw a short red diagonal on the canvas
+        Function sine = (x) -> {return (200 * Math.sin(Math.PI * ((double) x / 360)));};
+    	Function quad = (x) -> {return ((x-200) * (x+200)) / 250;};
+    	Function log = (x) -> {return (20 * Math.log(x));};
+    	Function cubic = (x) -> {return (Math.pow(x, 3) / 200000);};
+    	Function step = (x) -> {return x <= -100 ? -100 : x >= 100 ? 100 : x;};
+        
+        //Function[] funcs = {sine, quad, log, cubic};
+        
+        plotFunctions(sine, quad, log, cubic, step);
+
+        // Pause and close the canvas then terminate the program.
+        canvas.pause();
+        canvas.close();
+    }
+    
+    // plots various functions
+    public static void plotFunctions(Function... funcs) {
+    	
+    	/*Function sine = (x) -> {return (200 * Math.sin(Math.PI * ((double) x / 360)));};
+    	Function quad = (x) -> {return ((x-200) * (x+200)) / 250;};
+    	Function log = (x) -> {return (20 * Math.log(x));};
+    	Function cubic = (x) -> {return (Math.pow(x, 3) / 200000);};*/
+    	
+    	 // Draw a short red diagonal on the canvas
         canvas.pause();
         canvas.setColor(Color.red);
         for (int i = 0; i < 100; i++) {
             canvas.plot(i, i);
         }
-
-        // Pause and close the canvas then terminate the program.
+        
+        // Draws a sine function
         canvas.pause();
-        canvas.close();
+        canvas.setColor(Color.blue);
+        for (int i = -360; i <= 360; i++) {
+        	canvas.plot(i, (int) funcs[0].calc(i));
+        }
+        
+        // Draws a quadratic function
+        canvas.pause();
+        canvas.setColor(Color.green);
+        for (int i = -360; i <= 360; i++) {
+        	canvas.plot(i, (int) funcs[1].calc(i));
+        }
+        
+        // Draws a log function
+        canvas.pause();
+        canvas.setColor(Color.magenta);
+        for (int i = -360; i <= 360; i++) {
+        	canvas.plot(i, (int) funcs[2].calc(i));
+        }
+        
+        // Draws a cubic function
+        canvas.pause();
+        canvas.setColor(Color.black);
+        for (int i = -360; i <= 360; i++) {
+        	canvas.plot(i, (int) funcs[3].calc(i));
+        }
+        
+        // Draws a step function
+        canvas.pause();
+        canvas.setColor(Color.orange);
+        for (int i = -360; i <= 360; i++) {
+        	canvas.plot(i, (int) funcs[4].calc(i));
+        }
+    	
     }
 }
